59faba0e1ae39e777ee5e31cc97816279a716758
unknown <voranamra625@gmail.com>
2025-06-19 19:05:05 +0530

    blog ui update

--- frontend/src/components/FullBlog.tsx
+++ frontend/src/components/FullBlog.tsx
@@ -1,4 +1,3 @@
-
 import type { Blog } from '../hooks/useBlogs';
 import { Appbar } from './Appbar';
 import { Avatar } from './BlogCard';
@@ -9,7 +8,6 @@ const formatContent = (content: string) => {
         .split('\n\n')
         .map(paragraph => `<p class="text-lg leading-relaxed mb-4">${paragraph}</p>`)
         .join('');
-
     return formattedContent;
 };
 
@@ -17,20 +15,26 @@ export const FullBlog = ({ blog }: { blog: Blog }) => {
     return (
         <div>
             <Appbar />
-            <div className="flex justify-center mx-40 mt-20">
-                <div className="grid grid-cols-12 w-full max-w-screen-2xl">
+            <div className="flex justify-center px-4 sm:px-6 lg:px-20 mt-10">
+                <div className="grid grid-cols-1 lg:grid-cols-12 w-full max-w-screen-xl gap-y-10 gap-x-10">
 
-                    <div className="col-span-8">
-                        <h1 className="text-5xl font-extrabold">{blog.title}</h1>
-                        <p className="text-slate-500 pt-4">Posted on 2nd Nov 2024</p>
+                    {/* Blog Content */}
+                    <div className="lg:col-span-8 col-span-12">
+                        <h1 className="text-3xl sm:text-4xl lg:text-5xl font-extrabold">
+                            {blog.title}
+                        </h1>
+                        <p className="text-slate-500 pt-2 text-sm sm:text-base">
+                            Posted on 2nd Nov 2024
+                        </p>
                         <div
                             className="pt-4 text-justify"
                             dangerouslySetInnerHTML={{ __html: formatContent(blog.content) }}
                         ></div>
                     </div>
 
-                    <div className="col-span-4 pl-6">
-                        <h2 className="text-slate-600 text-lg">Author</h2>
+                    {/* Author Sidebar */}
+                    <div className="lg:col-span-4 col-span-12 lg:pl-6">
+                        <h2 className="text-slate-600 text-lg mb-2">Author</h2>
                         <div className="flex items-center space-x-2">
                             <Avatar name={blog.author.name || "Anonymous"} />
                             <span className="text-xl font-bold">
@@ -46,6 +50,6 @@ export const FullBlog = ({ blog }: { blog: Blog }) => {
             </div>
         </div>
     );
-}
+};
 
 export default FullBlog;
